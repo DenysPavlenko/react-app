0f3c5fe84c54fb99b55a336d41079108b2e19426
Denys Pavlenko <de.pavlenko@gmail.com>
2020-11-16 12:04:54 +0200

    responsive styles were added to the header. Mobile meny was added

---
+++ src/components/header-menu/header-menu.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+// Components
+import Burger from 'components/burger/burger';
+import Image from 'components/image/image';
+// Assets
+import cupIcon from 'assets/images/icons/cup.png'
+import timerIcn from 'assets/images/icons/timer.png'
+import cardsIcn from 'assets/images/icons/cards.png'
+import horseIcn from 'assets/images/icons/horse.png'
+import scoresIcn from 'assets/images/icons/scores.png'
+// Styles
+import './header-menu.sass';
+
+const menu = [
+  { name: 'Sports', rootName: '/sports', icon: cupIcon, alt: "sports" },
+  { name: 'Live', rootName: '/live', icon: timerIcn, alt: "live" },
+  { name: 'Casino', rootName: '/casino', icon: cardsIcn, alt: "casino" },
+  { name: 'Horses', rootName: '/horses', icon: horseIcn, alt: "horses" },
+];
+
+const HeaderMenu = () => {
+  const [scoresActive, toggleScoresActive] = useState(false);
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+
+  const handleScoresClick = () => {
+    toggleScoresActive(!scoresActive);
+  };
+
+  const handleToggleMenu = () => {
+    setIsMenuOpened(!isMenuOpened);
+  };
+
+  return (
+    <div className="header-menu">
+      <div className="header-menu__burger">
+        <Burger onClick={handleToggleMenu} />
+      </div>
+      <div className={`header-menu__wrap ${isMenuOpened ? 'is-active' : ''}`}>
+        <div className="header-menu__items">
+          {menu.map(({ name, rootName, icon }, idx) => (
+            <NavLink key={idx} to={rootName} exact={rootName === '/' && true} className="header-menu__item" onClick={handleToggleMenu}>
+              <span className="header-menu__text">{name}</span>
+              <Image src={icon} className="header-menu__icon" alt="nav-icon" icon />
+            </NavLink>
+          ))}
+        </div>
+      </div>
+      <div className={`header-menu__scores ${scoresActive ? 'is-active' : ''}`} onClick={handleScoresClick}>
+        <span className="header-menu__text">Scores</span>
+        <Image src={scoresIcn} className="header-menu__icon" alt="nav-icon" icon />
+      </div>
+    </div>
+  );
+};
+
+export default HeaderMenu;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 // Components
 import UserDropdown from 'components/user-dropdown/user-dropdown';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import BalanceOverview from 'components/balance-overview/balance-overview';
+import HeaderMenu from 'components/header-menu/header-menu';
 // Styles
 import './header.sass';
-// Assets
-import cup from 'assets/images/icons/cup.png'
-import timer from 'assets/images/icons/timer.png'
-import cards from 'assets/images/icons/cards.png'
-import horse from 'assets/images/icons/horse.png'
-import scores from 'assets/images/icons/scores.png'
-
-const menu = [
-  { name: 'Sports', rootName: '/sports', icon: cup },
-  { name: 'Live', rootName: '/live', icon: timer },
-  { name: 'Casino', rootName: '/casino', icon: cards },
-  { name: 'Horses', rootName: '/horses', icon: horse },
-  { name: 'Scores', rootName: '/scores', icon: scores },
-];
 
 const Header = () => {
   return (
@@ -27,12 +12,7 @@ const Header = () => {
       <div className="header-gap"></div>
       <div className="header">
         <div className="header__menu">
-          {menu.map(({ name, rootName, icon }, idx) => (
-            <NavLink key={idx} to={rootName} exact={rootName === '/' && true} className="header__menu-item">
-              <span className="header__menu-text">{name}</span>
-              <img src={icon} className="header__menu-icon" alt="nav-icon"/>
-            </NavLink>
-          ))}
+          <HeaderMenu />
         </div>
         <div className="header__balance">
           <BalanceOverview noFreePlay shrinkOnMobile />
