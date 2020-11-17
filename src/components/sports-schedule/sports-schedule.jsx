9616215c3548781a0d0ed30f8d8eb0c7ddfbd5d5
Denys Pavlenko <de.pavlenko@gmail.com>
2020-11-17 17:39:37 +0200

    useState was removed from sports-schedule

--- src/components/sports-schedule/sports-schedule.jsx
+++ src/components/sports-schedule/sports-schedule.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import PropTypes from 'prop-types';
