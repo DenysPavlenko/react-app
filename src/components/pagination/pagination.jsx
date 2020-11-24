759a571183499dec90093fba543fc62592ecf1ec
Denys Pavlenko <de.pavlenko@gmail.com>
2020-11-24 12:50:10 +0200

    colour scheming was removed from pagination component

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-// Redux
-import { selectColorScheme } from 'redux/color-scheme/selectors';
 // Styles
 import './pagination.sass';
 
-const Pagination = ({ pages, defaultColorScheme }) => {
+const Pagination = ({ pages, className }) => {
   const classes = classNames({
     'pagination': true,
-    [`pagination--${defaultColorScheme}`]: defaultColorScheme
+    [className]: className
   });
 
   return (
     <div className={classes}>
       <div className="pagination__wrap">
         {[...Array(pages)].map((i, idx) => (
-          <div key={idx} className="pagination__item">{idx +1}</div>
+          <div key={idx} className="pagination__item">{idx + 1}</div>
         ))}
       </div>
     </div>
@@ -27,11 +23,7 @@ const Pagination = ({ pages, defaultColorScheme }) => {
 
 Pagination.propTypes = {
   pages: PropTypes.number,
-  defaultColorScheme: PropTypes.string,
+  className: PropTypes.string,
 };
 
-const mapStateToProps = createStructuredSelector({
-  defaultColorScheme: selectColorScheme,
-});
-
-export default connect(mapStateToProps)(Pagination);
+export default Pagination;
