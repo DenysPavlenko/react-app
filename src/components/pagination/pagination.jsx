fd0b1fe05d47c49c63f735e1818b301400d5bcb7
Denys Pavlenko <de.pavlenko@gmail.com>
2020-11-24 14:38:58 +0200

    colors were refactored across all pages

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+// Redux
+import { selectColorScheme } from 'redux/color-scheme/selectors';
 // Styles
 import './pagination.sass';
 
-const Pagination = ({ pages, className }) => {
+const Pagination = ({ pages, defaultColorScheme }) => {
   const classes = classNames({
     'pagination': true,
-    [className]: className
+    [`pagination--${defaultColorScheme}`]: defaultColorScheme
   });
 
   return (
     <div className={classes}>
       <div className="pagination__wrap">
         {[...Array(pages)].map((i, idx) => (
-          <div key={idx} className="pagination__item">{idx + 1}</div>
+          <div key={idx} className="pagination__item">{idx +1}</div>
         ))}
       </div>
     </div>
@@ -23,7 +27,11 @@ const Pagination = ({ pages, className }) => {
 
 Pagination.propTypes = {
   pages: PropTypes.number,
-  className: PropTypes.string,
+  defaultColorScheme: PropTypes.string,
 };
 
-export default Pagination;
+const mapStateToProps = createStructuredSelector({
+  defaultColorScheme: selectColorScheme,
+});
+
+export default connect(mapStateToProps)(Pagination);
