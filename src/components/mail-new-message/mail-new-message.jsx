1ef8c845cee1fe63e43973448fc301dd885e72ea
Denys Pavlenko <de.pavlenko@gmail.com>
2020-12-02 14:50:22 +0200

    form validation added to mail-new-message component

--- src/components/mail-new-message/mail-new-message.jsx
+++ src/components/mail-new-message/mail-new-message.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
+// Helpers
+import isInputValid from 'helpers/is-input-valid';
 // Components
+import Form from 'components/form/form';
 import Typography from 'components/typography/typography';
 import Textarea from 'components/textarea/textarea';
 import Input from 'components/input/input';
@@ -7,34 +10,68 @@ import Button from 'components/button/button';
 // Styles
 import './mail-new-message.sass';
 
-const MessageNew = () => {
-  const [data, setData] = useState({
-    to: 'My agent',
-    subject: '',
-    message: ''
-  });
+const initialForm = {
+  to: '',
+  toInvalid: false,
+  subject: '',
+  subjectInvalid: false,
+  message: '',
+  messageInvalid: false,
+  formErrors: false
+};
 
-  const handleInput = ({ target: { value, name } }) => setData(data => ({ ...data, [name]: value }));
+const MessageNew = () => {
+  const [data, setData] = useState(initialForm);
+
+  const handleInput = ({ target: { name, type, value } }) => {
+    setData(data => {
+      return {
+        ...data,
+        [name]: value,
+        [`${name}Invalid`]: data.formErrors && !isInputValid(type, value),
+      }
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const toInvalid = !isInputValid('text', to);
+    const subjectInvalid = !isInputValid('text', subject);
+    const messageInvalid = !isInputValid('text', message);
+    setData(data => ({
+      ...data,
+      toInvalid,
+      subjectInvalid,
+      messageInvalid,
+      formErrors: toInvalid || subjectInvalid || messageInvalid
+    }));
+    if (!toInvalid && !subjectInvalid && !messageInvalid) {
+      setData(() => (initialForm))
+      console.log(1);
+    }
+  };
+
+  const { to, toInvalid, subject, subjectInvalid, message, messageInvalid } = data;
 
   return (
-    <div className="mail-new-message">
+    <Form className="mail-new-message" onSubmit={handleSubmit}>
       <div className="mail-new-message__header">
         <div className="mail-new-message__header-line">
           <Typography component="p" className="mail-new-message__header-title">To:</Typography>
-          <Input className="mail-new-message__header-input" value={data.to} name="to" type="text" onChange={handleInput} size="sm" fluid variant="transparent" />
+          <Input className="mail-new-message__header-input" value={to} isInvalid={toInvalid} name="to" type="text" onChange={handleInput} size="sm" fluid variant="primary-light" />
         </div>
         <div className="mail-new-message__header-line">
           <Typography component="p" className="mail-new-message__header-title">Subject:</Typography>
-          <Input className="mail-new-message__header-input" value={data.subject} name="subject" type="text" onChange={handleInput} size="sm" fluid variant="transparent" />
+          <Input className="mail-new-message__header-input" value={subject} isInvalid={subjectInvalid} name="subject" type="text" onChange={handleInput} size="sm" fluid variant="primary-light" />
         </div>
       </div>
       <div className="mail-new-message__body">
-        <Textarea rows={10} variant="primary-light" name="message" value={data.message} onChange={handleInput} />
+        <Textarea rows={10} variant="primary-light" name="message" value={message} isInvalid={messageInvalid} onChange={handleInput} />
       </div>
       <div className="mail-new-message__footer">
-        <Button variant="accent-blue" size="sm">Send</Button>
+        <Button variant="accent-blue" size="sm" type="submit">Send</Button>
       </div>
-    </div>
+    </Form>
   );
 };
 
