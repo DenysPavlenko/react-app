a24535bb73721fcec2f6b10c2bf9e2692cc4829c
Denys Pavlenko <de.pavlenko@gmail.com>
2020-12-03 13:40:12 +0200

    accordion animation was fixed

--- src/components/accordion/accordion-content/accordion-content.jsx
+++ src/components/accordion/accordion-content/accordion-content.jsx
@@ -1,23 +1,41 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 // Styles
 import './accordion-content.sass'
 
 const AccordionContent = ({ children, className, isExpanded }) => {
-  const content = useRef(null);
-  const [maxHeight, setMaxHeight] = useState(0);
+  const contentRef = useRef(null);
 
   useEffect(() => {
-    setMaxHeight(content.current.scrollHeight);
-  }, [setMaxHeight]);
+    const content = contentRef.current;
+    if (isExpanded) {
+      content.style.maxHeight = content.scrollHeight + 'px';
+      content.addEventListener('transitionend', clearMaxHeight);
+    }
+    return () => {
+      if (isExpanded) {
+        content.removeEventListener('transitionend', clearMaxHeight);
+        content.style.maxHeight = content.scrollHeight + 'px';
+        const timeout = setTimeout(() => {
+          content.style.maxHeight = '0';
+          clearTimeout(timeout);
+        }, 0);
+      }
+    }
+  }, [isExpanded]);
+
+  const clearMaxHeight = () => {
+    const content = contentRef.current;
+    content.style.maxHeight = 'none';
+  }
 
   const classes = classNames({
     [className]: className,
   });
 
   return (
-    <div ref={content} className="accordion-content" style={{ 'maxHeight': `${isExpanded ? maxHeight : '0'}px` }}>
+    <div ref={contentRef} className="accordion-content" >
       <div className={classes}>
         {children}
       </div>
