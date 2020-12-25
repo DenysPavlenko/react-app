e8ee37a6a7047d9efed4918d429107fc69d4ab6d
Denys Pavlenko <de.pavlenko@gmail.com>
2020-12-25 14:02:27 +0200

    position-live-sports component refactored, position-live-sports table-content refactored, position-live-sports dummy-data refactored,

--- src/admin-app/components/position-live-sports/position-live-sports.jsx
+++ src/admin-app/components/position-live-sports/position-live-sports.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from 'react';
+import React, { Fragment, useLayoutEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
@@ -6,29 +6,44 @@ import { createStructuredSelector } from 'reselect'
 import { fetchPositionLiveSportsData } from 'admin-app/redux/position-live-sports/actions';
 import { selectPositionLiveSports } from 'admin-app/redux/position-live-sports/selectors';
 // Components
-import PrimaryTable from 'shared/components/primary-table/primary-table';
+import PositionTable from 'admin-app/components/position-table/position-table';
+import PendingBets from 'admin-app/components/pending-bets/pending-bets';
 // Table content
 import tableContent from './table-content';
 // Styles
 import './position-live-sports.sass';
 
 const PositionLiveSports = ({ fetchPositionLiveSportsData, positionLiveSports: { loading, data, error } }) => {
+  const [openModal, setOpenModal] = useState(false);
+  const [selectedAgent, setSelectedAgent] = useState('');
 
   useLayoutEffect(() => {
     fetchPositionLiveSportsData();
   }, [fetchPositionLiveSportsData])
 
+  const handleAgentSelect = agent => {
+    setOpenModal(true);
+    setSelectedAgent(agent);
+  };
+
   return (
-    <div className="position-live-sports">
-      <PrimaryTable
-        cols={tableContent()}
-        loading={loading}
-        data={data}
-        error={error}
-        retry={fetchPositionLiveSportsData}
-        variant="primary"
+    <Fragment>
+      <PendingBets
+        open={openModal}
+        agent={selectedAgent}
+        onClose={() => setOpenModal(false)}
+        onExited={() => setSelectedAgent('')}
       />
-    </div>
+      <div className="position-live-sports">
+        <PositionTable
+          cols={tableContent(handleAgentSelect)}
+          loading={loading}
+          data={data}
+          error={error}
+          retry={fetchPositionLiveSportsData}
+        />
+      </div>
+    </Fragment>
   );
 };
 
--- src/admin-app/components/position-live-sports/table-content.js
+++ src/admin-app/components/position-live-sports/table-content.js
@@ -1,26 +1,67 @@
 import React from 'react';
 // Components
 import Typography from 'shared/components/typography/typography';
-// Utils
-import setDangerClass from 'shared/utils/set-danger-class';
+import ActionIcon from 'shared/components/action-icon/action-icon';
 
-const tableContent = () => {
+const tableContent = handleAgentSelect => {
+  const isOdd = idx => idx % 2 === 0;
   return [
     {
-      title: 'Date',
-      render: data => <Typography component="p">{data.date}</Typography>
+      render: ({ id }, idx) => isOdd(idx) ? <ActionIcon icon="search" onClick={() => handleAgentSelect(id)} /> : null
     },
     {
-      title: 'Rotation',
-      render: data => <Typography component="p">{data.rotation}</Typography>
+      render: ({ date, time }, idx) => <Typography component="p">{isOdd(idx) ? date : time}</Typography>
     },
     {
-      title: 'Game',
-      render: data => <Typography component="p">{data.game}</Typography>
+      render: ({ rotation }) => <Typography component="p">{rotation}</Typography>
     },
     {
-      title: 'Straight',
-      render: data => <Typography component="p" className = { setDangerClass() }>{data.straight}</Typography>
+      render: ({ game }) => <Typography component="p">{game}</Typography>
+    },
+    {
+      render: ({ strPs1 }) => <Typography component="p">{strPs1}</Typography>
+    },
+    {
+      render: ({ strPs2 }) => <Typography component="p">{strPs2}</Typography>
+    },
+    {
+      render: ({ strMl1 }) => <Typography component="p">{strMl1}</Typography>
+    },
+    {
+      render: ({ strMl2 }) => <Typography component="p">{strMl2}</Typography>
+    },
+    {
+      render: ({ strTot1 }) => <Typography component="p">{strTot1}</Typography>
+    },
+    {
+      render: ({ strTot2 }) => <Typography component="p">{strTot2}</Typography>
+    },
+    {
+      render: ({ strTto }) => <Typography component="p">{strTto}</Typography>
+    },
+    {
+      render: ({ strTto2 }) => <Typography component="p">{strTto2}</Typography>
+    },
+    {
+      render: ({ strTtu1 }) => <Typography component="p">{strTtu1}</Typography>
+    },
+    {
+      render: ({ strTtu2 }) => <Typography component="p">{strTtu2}</Typography>
+    },
+    {
+      render: ({ parPs }) => <Typography component="p">{parPs}</Typography>
+    },
+    {
+      render: ({ parMl }) => <Typography component="p">{parMl}</Typography>
+    },
+    {
+      render: ({ parTot }) => <Typography component="p">{parTot}</Typography>
+    },
+    {
+      render: ({ teasPs }) => <Typography component="p">{teasPs}</Typography>
+    },
+    {
+      render: ({ teasTot }) => <Typography component="p">{teasTot}</Typography>
     },
   ];
 };
