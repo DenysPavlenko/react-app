21ffbabfb5a48b4a6ba6fd64d18f439890bd277a
Denys Pavlenko <de.pavlenko@gmail.com>
2020-12-31 19:34:08 +0200

    Refactor accordion-tab and accordion-tab.sass - color schemes were added

--- src/shared/components/accordion-tab/accordion-tab.jsx
+++ src/shared/components/accordion-tab/accordion-tab.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import classNames from 'classnames';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import PropTypes from 'prop-types';
+// Redux
+import { selectColorScheme } from 'shared/redux/color-scheme/selectors';
 // Components
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Typography from 'shared/components/typography/typography';
 // Styles
 import './accordion-tab.sass';
 
-const AccordionTab = ({ icon, title, counter, isActive, withBorder, onClick }) => {
+const AccordionTab = ({ icon, title, counter, isActive, colorScheme, withBorder, onClick }) => {
   const classes = classNames({
     'accordion-tab': true,
     'accordion-tab--with-border': withBorder,
+    [`theme-${colorScheme}`]: isActive && colorScheme,
     'is-active': isActive,
   });
 
@@ -28,7 +33,12 @@ AccordionTab.propTypes = {
   title: PropTypes.string,
   counter: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   isActive: PropTypes.bool,
+  colorScheme: PropTypes.string,
   onClick: PropTypes.func,
 };
 
-export default AccordionTab;
+const mapStateToProps = createStructuredSelector({
+  colorScheme: selectColorScheme,
+});
+
+export default connect(mapStateToProps, null)(AccordionTab);
