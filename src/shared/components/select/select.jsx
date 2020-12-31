1cac49c1e19a165d5778537d204c53a41216407a
Denys Pavlenko <de.pavlenko@gmail.com>
2020-12-31 15:42:02 +0200

    Refactor select component - styles folder added - select sizes, variants and types styles were separated - colorScheme were added to the select component

--- src/shared/components/select/select.jsx
+++ src/shared/components/select/select.jsx
@@ -2,15 +2,20 @@ import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { CSSTransition } from 'react-transition-group';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+// Redux
+import { selectColorScheme } from 'shared/redux/color-scheme/selectors';
 // Hooks
 import useOnClickOutside from 'shared/hooks/useClickOutside'
 // Components
 import Button from 'shared/components/button/button';
 import Simplebar from 'simplebar-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // Styles
 import './select.sass';
 
-const Select = ({ options, name, value, placeholder, inline, onChange, fluid, variant, size, className, style }) => {
+const Select = ({ options, name, value, placeholder, inline, onChange, fluid, variant, size, className, style, colorScheme }) => {
   const selectDopdownRef = useRef(null);
   const selectRef = useRef(null);
   const selectOptionsRef = useRef(null);
@@ -53,6 +58,7 @@ const Select = ({ options, name, value, placeholder, inline, onChange, fluid, va
     [`select--${variant}`]: variant,
     [`select--${size}`]: size,
     'select--fluid': fluid,
+    [`theme-${colorScheme}`]: colorScheme,
     [className]: className
   });
 
@@ -62,6 +68,7 @@ const Select = ({ options, name, value, placeholder, inline, onChange, fluid, va
     <div ref={selectRef} className={classes} onClick={handleSelectClick}>
       <Button className="select__label" standard={false} style={style}>
         {selectedValue !== null ? getLabel(selectedValue) : placeholder}
+        <FontAwesomeIcon className="select__label-caret" icon="caret-down" />
       </Button>
       <CSSTransition nodeRef={selectDopdownRef} in={isExpanded} timeout={300} onEnter={handleOnEnter} unmountOnExit classNames="select-box-animation">
         <div ref={selectDopdownRef} className="select__dropdown">
@@ -83,7 +90,7 @@ const Select = ({ options, name, value, placeholder, inline, onChange, fluid, va
       </CSSTransition>
     </div>
   )
-}
+};
 
 Select.defaultProps = {
   placeholder: '',
@@ -102,4 +109,8 @@ Select.propTypes = {
   style: PropTypes.object,
 };
 
-export default Select;
+const mapStateToProps = createStructuredSelector({
+  colorScheme: selectColorScheme
+});
+
+export default connect(mapStateToProps)(Select);
