b801a9f1119e646f37e5fe47503b182e1f933ba1
Denys Pavlenko <de.pavlenko@gmail.com>
2021-02-11 20:02:59 +0200

    Refactor get-dummy-data function

--- src/services/_utils/get-dummy-data.js
+++ src/services/_utils/get-dummy-data.js
@@ -1,9 +1,10 @@
-const getDummyData = async ({data, errorMsg = 'error', errorProbability = 0.99, timeOut = 500}) => {
-  const res = await data.default;
-  await new Promise((res, rej) => setTimeout(() => {
-    return Math.random() > errorProbability ? rej(new Error(errorMsg)) : res();
-  }, timeOut));
-  return res;
+const getDummyData = async ({ data, errorMsg = 'error', errorProbability = 0.99, timeOut = 500 }) => {
+  const actualData = data.default;
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      Math.random() > errorProbability ? rej(new Error(errorMsg)) : res(actualData);
+    }, timeOut);
+  });
 };
 
 export default getDummyData;
